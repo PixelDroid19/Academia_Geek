bd6a9e4f8e7b37edb3c4aceeaa386ceec65b4f90
Damien Monasterios <monasterio13septiembre@gmail.com>
2021-08-08 17:56:40 -0500

    Update

---
+++ Modulo 2/06-Eventos/Ejercicio 1/script.js
@@ -0,0 +1,32 @@
+/*Ejercicio 1
+Capturar tres números por medio de cajas de texto, compararlos e indicar cual número es mayor y cual menor. 
+Adicionar una validación en caso de que sean iguales. Implementar el evento DOMContentLoaded y click.*/
+let Result = document.getElementById("result");
+
+let button = document.getElementById("btn"),
+      number1 = 0,
+      number2 = 0,
+      number3 = 0;
+
+function getData(){
+    number1 = parseInt(document.getElementById("number1").value),
+    number2 = parseInt(document.getElementById("number2").value),
+    number3 = parseInt(document.getElementById("number3").value);
+
+    comparison();
+}
+
+function comparison(){
+    if(number1 > number2 && number1 > number3){
+        Result.innerHTML = "El mayor es : " + number1;
+    }else if(number2 > number1 && number2 > number3){
+        Result.innerHTML = "El mayor es : " + number2;
+    }else if(number3 > number1 && number3 > number2){
+        Result.innerHTML = "El mayor es : " + number3;
+    }else{
+        Result.innerHTML = "Son iguales";
+    }
+}
+
+
+button.onclick = getData;
