f4f671baf5efb419411171c447282a68fed1c34c
Damien Monasterios <monasterio13septiembre@gmail.com>
2021-08-08 12:46:04 -0500

    Update

---
+++ Modulo 2/08-Arreglos/Ejercicio 1/script.js
@@ -0,0 +1,9 @@
+/*Ejercicio 1
+Crear un arreglo (temática libre), el cual sea recorrido con la función map()
+ e imprimir por consola cada uno de sus elementos.*/
+ let Vector = [1, 2, 3, 4, 5];
+ 
+let recorrerVector = Vector.map(function(valor){
+    console.log(valor);
+});
+ 
